8d86e95150ff98f43e0809b6df374ab7cea19eb1
daab36 <daab36@gmail.com>
2025-01-16 14:57:33 -0600

    mistake of living

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, {StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
     <App />
-)
+  </StrictMode>
+);
